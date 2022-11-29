1c6ef9df3f85b3dff391dec9bd61aacfbf9c4ef2
Faezragore <e.v.tarasevich@yandex.ru>
2022-11-29 22:23:48 +0300

    update3

--- jenkins/test.groovy
+++ jenkins/test.groovy
@@ -1,16 +1,16 @@
-def remote = [:]
-remote.name = "node"
-remote.host = credentials('ip_adress')
-remote.allowAnyHosts = true
+node('my_server_linux') {
+    stage('test') {
+        name = "test1"
+        host = credentials('ip_adress')
+        allowAnyHosts = true
 
-node {
-    withCredentials([usernamePassword(credentialsId: 'my_server_ruvds.com', passwordVariable: 'password_server', usernameVariable: 'user_server')]) {
-        remote.user = user_server
-        remote.password = password_server
-        echo "$remote.user"
-        echo "$remote.password"
-        stage("SSH Steps Rocks!") {
-            sh "ls -al"
-        }
+            withCredentials([usernamePassword(credentialsId: 'my_server_ruvds.com', passwordVariable: 'password_server', usernameVariable: 'user_server')]) {
+                user = user_server
+                password = password_server
+                echo "$user"
+                stage("SSH Steps Rocks!") {
+                    sh "ls -al"
+                }
+            }
     }
 }
